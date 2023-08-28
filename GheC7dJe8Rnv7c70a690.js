e756316772d9c8a3c1f57583111898a3dbf21532
Sachin Sinha <sachin@iqlect.com>
2023-08-28 14:00:27 +0530

    Update existing JavaScript file

--- GheC7dJe8Rnv7c70a690.js
+++ GheC7dJe8Rnv7c70a690.js
@@ -1,6 +1,6 @@
 
 // Array of form IDs to target
-const formIdsToTarget = [ "form-register", "form-login" ];
+const formIdsToTarget = [ "form-register", "form-login", "update-this" ];
 
 // Function to collect form data and create objects
 function collectFormData(event) {
