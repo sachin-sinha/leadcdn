68e4bfae874a35bb70a72b881b2d1c58a9dbae49
Sachin Sinha <sachin@iqlect.com>
2023-08-28 12:48:18 +0530

    Update existing JavaScript file

--- GheC7dJe8Rnv7c70a690.js
+++ GheC7dJe8Rnv7c70a690.js
@@ -13,11 +13,11 @@ function collectFormData(event) {
     formObject[key] = value;
   });
 
-  fetch('API_ENDPOINT', {
+  fetch('https://testbe.bangdb.com:18080/stream/7c70a690_Leads_Management_base/Data', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'x-bang-api-key': 'API_KEY'
+      'x-bang-api-key': '1923072136593821767'
     },
     body: JSON.stringify(formObject)
   })
